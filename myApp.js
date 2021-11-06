0811efcfe7f786cc75608815cda4ec1cdf061301
Wanuja Ransinghe <wanuja18@gmail.com>
2021-11-07 02:10:55 +0530

    Created many records with Model.create

--- myApp.js
+++ myApp.js
@@ -12,15 +12,67 @@ const personSchema = new Schema({
 // Creating Model
 const Person = mongoose.model("Person",personSchema);
 
+// Create and save record from model
+var createAndSavePerson = function (done) {
+  var janeFonda = new Person({ name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"] });
 
-// let Person;
+  janeFonda.save(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+
+// =============================================================================================
+
+// Create Many Models with Model.Create
+/** 1) Install & Set up mongoose */
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+/** 2) Create a 'Person' Model */
+var personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+});
+
+/** 3) Create and Save a Person */
+var Person = mongoose.model('Person', personSchema);
+
+var createAndSavePerson = function (done) {
+  var janeFonda = new Person({ name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"] });
+
+  janeFonda.save(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
 };
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+
+/** 4) Create many People with `Model.create()` */
+var arrayOfPeople = [
+  { name: "Frankie", age: 74, favoriteFoods: ["Del Taco"] },
+  { name: "Sol", age: 76, favoriteFoods: ["roast chicken"] },
+  { name: "Robert", age: 78, favoriteFoods: ["wine"] }
+];
+
+var createManyPeople = function (arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
+
+// =============================================================================================
+
+
+
+
+
+
+
+
+
 
 const findPeopleByName = (personName, done) => {
   done(null /*, data*/);
